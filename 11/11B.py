d8b58ccd0f09370a85ff46e50ceb93ff2e417165
bluezebragames <bluezebragames@gmail.com>
2017-12-14 02:05:52 -0700

    Day 11

---
+++ 11/11B.py
@@ -0,0 +1,32 @@
+import sys
+
+directions = []
+for line in sys.stdin:
+    line = line.strip()
+    directions = line.split(",")
+
+x=y=z=0
+max_dist = 0
+for direction in directions:
+    # see http://keekerdc.com/wp-content/uploads/2011/03/HexGridLandscapeTriCoordinates.gif
+    if direction == 'n':
+        y += 1
+        z -= 1
+    if direction == 's':
+        y -= 1
+        z += 1
+    if direction == 'ne':
+        x += 1
+        z -= 1
+    if direction == 'sw':
+        x -= 1
+        z += 1
+    if direction == 'nw':
+        x -= 1
+        y += 1
+    if direction == 'se':
+        x += 1
+        y -= 1
+    max_dist = max(max_dist, max(x, max(y, z)))
+
+print(max_dist)
